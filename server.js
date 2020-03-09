16d9447340b70a4b515158f19ad4bb5acd76005e
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-03-09 09:52:33 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+//Create a COMPANY database
+// In the company database I need three tables: Employee, Role, and Department
+// In the Department table, I need two rows:  ID and name
+// In the Role table, I need: ID, title, Salary, and department_id
+// In the Employee Table, I need : ID, first name, last name, role_id, manager_id
+
+// I want to build a command-line app that allows user to :
+//   Add departments, roles, and employees
+//   View departments, roles, and employees
+//   Update employee roles
