4d2ac05571eb86dfcd14aff54e7f3eb347d98c35
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-03-10 09:31:07 -0500

    created connection to mysql

--- server.js
+++ server.js
@@ -11,3 +11,17 @@
 
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+var connection = mysql.createConnection({
+    host:"localhost",
+    PORT: 3306,
+    user: "root",
+    password: "",
+    database: "COMPANY_DB"
+
+});
+
+connection.connect(function(err){
+    if(err) throw(err);
+    console.log("Connection as id:" + connection.threadId)
+    connection.end();
+});
