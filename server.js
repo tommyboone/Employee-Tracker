8f027e49da80454987acfcb6229c06aada502040
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-03-12 17:29:02 -0500

    added add employee function

--- server.js
+++ server.js
@@ -41,30 +41,37 @@ const viewOptions = [
   "Update Employee",
   "Delete Employee",
   "Add Employee",
+  "Add Department",
+  "Add Role",
   "Exit"
 ];
 
-const employeeOptions = [
-  "Michael Scott",
-  "Jim Halpert",
-  "Dwight Schrute",
-  "Pam Beasley",
-  "Creed Bratton",
-  "Kelly Kapur"
-];
+// const employeeOptions = [
+//   "Michael Scott",
+//   "Jim Halpert",
+//   "Dwight Schrute",
+//   "Pam Beasley",
+//   "Toby Flenderson",
+//   "Kelly Kapur"
+// ];
 
-const roles = [
-  "Junior Salesperson",
-  "Senior Salesperson",
-  "Junior Engineer",
-  "Senior Engineer",
-  "Accountant",
-  "Head of Accounting",
-  "Attorney",
-  "Manager"
-];
+// const roles = [
+//   "Salesperson",
+//   "Customer Service",
+//   "Accountant",
+//   "Office Administrator",
+//   "HR Rep",
+//   "Manager"
+// ];
 
-const updateOptions = ["First Name", "Last Name", "Role", "Department", "Exit"];
+// const updateOptions = [
+//   "First Name",
+//   "Last Name",
+//   "Role",
+//   "Department",
+//   "Manager",
+//   "Exit"
+// ];
 
 function start() {
   inquirer
@@ -92,9 +99,16 @@ function start() {
           deleteEmployee();
           break;
         case viewOptions[5]:
-            addEmployee();
-            break;
+          addEmployee();
+          break;
         case viewOptions[6]:
+          addDepartment();
+          break;
+        case viewOptions[7]:
+          addRole();
+          break;
+        case viewOptions[8]:
+          console.log("Goodbye!");
           connection.end();
           break;
       }
@@ -119,7 +133,8 @@ function start() {
   }
 
   function viewEmployees() {
-    let sql = "SELECT first_name, last_name FROM employee ";
+    let sql =
+      "SELECT id, first_name, last_name, role_id, manager_id FROM employee ";
     connection.query(sql, function(err, res) {
       if (err) throw err;
       console.table(res);
@@ -127,36 +142,187 @@ function start() {
     });
   }
 
-  const updateEmployee = () => {
+  function updateEmployee() {
     function update() {
-      inquirer
-        .prompt({
-          name: "update",
-          type: "list",
-          message: "Which employee do you want to update?",
-          choices: employeeOptions
-        })
-        .then(function() {
-          inquirer
-            .prompt({
+      connection.query("SELECT * FROM employee", function(err, res) {
+        console.log(res);
+        var employeeList = [];
+        for (var i = 0; i < res.length; i++) {
+          employeeList.push(res[i].first_name);
+        }
+        inquirer
+          .prompt([
+            {
+              name: "update",
+              type: "list",
+              message: "Which employee do you want to update?",
+              choices: employeeList
+            },
+            {
               name: "updateOptions",
               type: "list",
-              message: "What would like to update?",
-              choices: updateOptions
-            })
-            .then(function(answer) {
-              switch (answer.updateOptions) {
-                case updateOptions[0]:
-                  updatefirstName();
-                  break;
+              message: "What would you like to update?",
+              choices: ["Role", "Manager"]
+            }
+          ])
+          .then(function(answer) {
+            connection.query("SELECT * FROM role", function(err, resRole){
+              console.log(resRole);
+              var roleList = [];
+              for(var i = 0; i < resRole.length; i++){
+                roleList.push(resRole[i].title)
               }
-            });
-        });
+              inquirer  
+                .prompt({
+                  name:"roleUpdate",
+                  type:"list",
+                  message: "what is the new role",
+                  choices: roleList
+                }).then(function(roleAnswer){
+                  console.log(roleAnswer)
+
+                  var roleID
+                  var empID 
+
+                  for(var i = 0; i< res.length; i++){
+                    if(answer.update === res[i].first_name){
+                      empID = res[i].first_name
+                    }
+                  }
+
+                  for (var i = 0; i < resRole.length; i++){
+                    if(answer.updateOptions === resRole[i].id){
+                      roleID= resRole[i].role_id
+                    }
+                  }
+                  connection.query("UPDATE employee SET role_id = ? WHERE first_name = ?", [roleID, empID],function(err,res){
+                    console.log(err, res);
+                  })
+                })
+              
+            })
+            // console.log(answer);
+          });
+      });
     }
-    function updatefirstName() {
-      let sql = "SELECT first_name FROM employees where first_name = ?";
-    }
+
     update();
-  };
+  }
+
+function deleteEmployee() {
+  connection.query("SELECT * FROM employee", function(err, res) {
+    console.log(err, res);
+    var employeeList = [];
+    for (var i = 0; i < res.length; i++) {
+      employeeList.push(res[i].first_name);
+    }
+    inquirer
+      .prompt({
+        name: "deletedEmployee",
+        type: "list",
+        message: "Which employee would you like to remove?",
+        choices: employeeList
+      })
+      .then(function(response) {
+        console.log(response);
+        var deleteID;
+        for (var i = 0; i < res.length; i++) {
+          if (response.deletedEmployee === res[i].first_name) {
+            deleteID = res[i].id;
+          }
+        }
+        connection.query(
+          `DELETE FROM employee where id = ${deleteID}`,
+          function(err, res) {
+            console.log(err, res);
+            start();
+          }
+        );
+      });
+     
+  });
+}
+
+// function deleteRole() {
+//   connection.query("SELECT * FROM role", function(err, res) {
+//     console.log(err, res);
+//     var roleList = [];
+//     for (var i = 0; i < res.length; i++) {
+//       roleList.push(res[i].title);
+//     }
+//     inquirer
+//       .prompt({
+//         name: "deletedRole",
+//         type: "list",
+//         message: "Which Rolewould you like to remove?",
+//         choices: roleList
+//       })
+//       .then(function(response) {
+//         console.log(response);
+//         var deleteRole;
+//         for (var i = 0; i < res.length; i++) {
+//           if (response.deletedRole === res[i].title) {
+//             deleteRole = res[i].id;
+//           }
+//         }
+//         connection.query(
+//           `DELETE FROM role where id = ${deleteRole}`,
+//           function(err, res) {
+//             console.log(err, res);
+//             start();
+//           }
+//         );
+//       });
+     
+//   });
+// }
+
+function addEmployee() {
+  connection.query("SELECT * FROM role", function(err, res) {
+    console.log(err, res);
+    var selectRole = [];
+    for (var i = 0; i < res.length; i++) {
+      selectRole.push(res[i].title);
+    }
+    inquirer
+      .prompt([
+        {
+          name: "firstName",
+          type: "input",
+          message: "What's your employees' first name?"
+        },
+        {
+          name: "lastName",
+          type: "input",
+          message: "What's your employees' last name?"
+        },
+        {
+          name: "employeeRole",
+          type: "list",
+          message: "What is this employees' role?",
+          choices: selectRole
+        }
+      ])
+      .then(function(answer) {
+        console.log(answer);
+        var roleid;
+        for (var i = 0; i < res.length; i++) {
+          if (answer.employeeRole === res[i].title) {
+            roleid = res[i].id;
+          }
+        }
+        connection.query(
+          "INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?)",
+          [answer.firstName, answer.lastName, roleid, 1],
+          function(err, res) {
+            console.table(err,res)
+            start();
+          }
+        );
+
+      });
+    })
+  }
+ 
 }
 
